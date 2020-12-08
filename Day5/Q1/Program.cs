617ce767aa4869393951d4c21003955f8d94ff03
vishvajit96 <65163156+vishvajit96@users.noreply.github.com>
2020-12-08 20:41:34 +0530

    Add files via upload
    Day5

---
+++ Day5/Q1/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03_Q1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter size");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Employee[] emp = new Employee[size];
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine("enter empNo");
+                int no = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("enter empName");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("enter basic salary");
+                decimal basic = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("enter deptNo");
+                int dep = Convert.ToInt32(Console.ReadLine());
+                Employee e = new Employee(no, name, basic, dep);
+                emp[i] = e;
+            }
+            decimal arr = emp[0].Pbasic;
+            for (int i = 1; i < emp.Length; i++)
+            {
+                if (arr < emp[i].Pbasic)
+                {
+                    arr = emp[i].Pbasic;
+                }
+            }
+            Console.WriteLine("highest salary is : " + arr);
+
+            Console.WriteLine("Enter Employee no to be searched");
+            int empNo = Convert.ToInt32(Console.ReadLine());
+            foreach (Employee e in emp)
+            {
+                if (e.PempNo == empNo)
+                {
+                    Console.WriteLine("Empno :=" + e.PempNo + " name=: " + e.Pname + " emp basic:= " + e.Pbasic + " depNo := " + e.PdeptNo);
+                }
+            }
+        }
+    }
+    public class Employee
+    {
+        private string name;
+        private int empNo;
+        private decimal basic;
+        private int deptNo;
+
+
+
+
+
+        public Employee(int empNo, string name, decimal basic, int depNo)
+        {
+
+            this.empNo = empNo;
+
+            this.name = name;
+            this.basic = basic;
+            this.deptNo = depNo;
+        }
+
+        #region property
+        public string Pname
+        {
+            set
+            {
+
+                if (value.Length != 0)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    Console.WriteLine("name not be null");
+                }
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        public int PempNo
+        {
+            get
+            {
+                return empNo;
+            }
+        }
+
+        public decimal Pbasic
+        {
+            set
+            {
+                if (value >= 25000 && value <= 100000)
+                {
+                    this.basic = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("enter basic between 25000 to 35000");
+                }
+            }
+            get
+            {
+                return basic;
+            }
+        }
+
+        public int PdeptNo
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.deptNo = value;
+                }
+                else
+                {
+                    Console.WriteLine("deptno must be above 0");
+                }
+            }
+            get
+            {
+                return deptNo;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Day5/Q3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter how many student want");
+            int size = Convert.ToInt32(Console.ReadLine());
+            student[] s = new student[size];
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine("Enter Student roll no");
+                int no = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Student name");
+                string name = Console.ReadLine();
+                ;
+                Console.WriteLine("Enter Student mark");
+                decimal mark = Convert.ToDecimal(Console.ReadLine());
+                student s1 = new student(no, name, mark);
+                s[i] = s1;
+            }
+            foreach (student a in s)
+            {
+                Console.WriteLine(a.Ino + " " + a.Pname + " " + a.Imark);
+            }
+        }
+    }
+    public struct student
+    {
+        string Name;
+        int RollNo;
+        decimal Marks;
+        public student(int rollno, string name, decimal mark)
+        {
+            this.Name = name;
+            this.RollNo = rollno;
+            this.Marks = mark;
+        }
+        public string Pname
+        {
+            set
+            {
+
+                if (value.Length != 0)
+                {
+                    this.Name = value;
+                }
+                else
+                {
+                    Console.WriteLine("name not be null");
+                }
+            }
+            get
+            {
+                return Name;
+            }
+        }
+        public int Ino
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.RollNo = value;
+                }
+
+            }
+            get
+            {
+                return RollNo;
+            }
+        }
+        public decimal Imark
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    this.Marks = value;
+                }
+            }
+            get
+            {
+                return Marks;
+            }
+        }
+
+    }
+}
